3a8d521d1732a4bbec2dcb56c5e2c9b332bc4d74
TerryLikesCoding <18328621355@163.com>
2023-05-30 07:24:22 +0800

    删除ExtensionList中无用部分

--- client/src/components/ExtensionList.vue
+++ client/src/components/ExtensionList.vue
@@ -1,15 +1,5 @@
 <template>
   <div>
-    <h1 class="my-4">Welcome to LiTools!</h1>
-    <div class="card-deck">
-      <div class="card mb-4">
-        <div class="card-body">
-          <h5 class="card-title">Extensions Gallery</h5>
-          <p class="card-text">Browse and search for LiTools extensions.</p>
-          <router-link class="btn btn-primary" to="/extensions">View Extensions</router-link>
-        </div>
-      </div>
-    </div>
     <div class="row">
       <div class="col-md-4" v-for="extension in extensions" :key="extension.id">
         <div class="card mb-4 box-shadow">
