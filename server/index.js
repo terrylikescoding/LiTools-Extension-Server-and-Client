733a28c0d3620d4edcf0769237991b2bec8de31c
TerryLikesCoding <18328621355@163.com>
2023-05-29 10:03:43 +0800

    提交文件，准备推送到github

---
+++ server/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+
+const app = express();
+const port = 3000;
+
+const db = new sqlite3.Database('./database.sqlite');
+
+// Middleware to parse request body
+app.use(express.json());
+
+// Get a list of all extensions
+app.get('/extensions', (req, res) => {
+  const query = 'SELECT * FROM extensions';
+  console.log('query123: '+query)
+  db.all(query, [], (err, rows) => {
+    if (err) {
+      res.status(500).send({ error: err.message });
+    } else {
+      res.send(rows);
+    }
+  });
+});
+
+// Get the latest version of an extension
+app.get('/extensions/:id/version', (req, res) => {
+  const query = 'SELECT version FROM extensions WHERE id = ?';
+  db.get(query, [req.params.id], (err, row) => {
+    if (err) {
+      res.status(500).send({ error: err.message });
+    } else {
+      res.send(row.version);
+    }
+  });
+});
+
+// Upload an extension and add it to the database
+app.post('/extensions/upload', (req, res) => {
+  const { name, version, description, createdBy } = req.body;
+  const query = 'INSERT INTO extensions (name, version, description, createdBy) VALUES (?, ?, ?, ?)';
+  db.run(query, [name, version, description, createdBy], function(err) {
+    if (err) {
+      res.status(500).send({ error: err.message });
+    } else {
+      res.send({ id: this.lastID });
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
