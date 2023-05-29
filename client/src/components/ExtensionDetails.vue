b0546832735af196c57a805ac84af789c534c71d
TerryLikesCoding <18328621355@163.com>
2023-05-29 22:22:05 +0800

    为EXtensionDetail添加异常处理

--- client/src/components/ExtensionDetails.vue
+++ client/src/components/ExtensionDetails.vue
@@ -8,11 +8,13 @@
   
   <script>
   import axios from 'axios';
- export default {
+  export default {
   props: ['id'],
   data() {
     return {
       extension: null,
+      loading: true,
+      error: null,
     };
   },
   created() {
@@ -23,13 +25,26 @@
     fetchExtension(id) {
       axios.get(`http://localhost:3000/extensions/${id}`).then(response => {
         this.extension = response.data;
+        this.loading = false;
       }).catch(error => {
         console.error(error);
+        this.loading = false;
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          this.error = error.response.data;
+        } else if (error.request) {
+          // The request was made but no response was received
+          this.error = 'Unable to connect to server';
+        } else {
+          // Something else happened while setting up the request
+          this.error = 'An error occurred while fetching extension details';
+        }
       });
     },
     onBack() {
       // Use the router to navigate back to the extensions list page
-      this.$router.push({ name: 'extensionsList' });
+      this.$router.push({ name: 'extensionList' });
     },
   },
 };
