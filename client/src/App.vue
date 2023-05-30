3bc0b529fdbc8c8d056d9b9106e215e65735e984
TerryLikesCoding <18328621355@163.com>
2023-05-30 14:02:58 +0800

    为页面添加footer

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="site-container">
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
       <div class="container">
         <a class="navbar-brand" href="#">LiTools</a>
@@ -16,15 +16,47 @@
         </div>
       </div>
     </nav>
-    <div class="container mt-3">
+    <div class="content">
       <router-view></router-view>
     </div>
+    <footer class="footer">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12 col-md-6 text-center text-md-left">
+            <span class="text-muted">Contact LiTools at xxx@yyy.com</span>
+          </div>
+          <div class="col-12 col-md-6 text-center text-md-right">
+            <span class="text-muted">LiTools 版权所有 © 2023</span>
+          </div>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
-
 <style>
 @import url('https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css');
+
+.site-container {
+  min-height: 100vh;
+  position: relative;
+}
+
+.content {
+  padding-bottom: 50px;
+}
+
+.footer {
+  background-color: #f5f5f5;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 0 15px;
+}
 </style>
 
 <script>
