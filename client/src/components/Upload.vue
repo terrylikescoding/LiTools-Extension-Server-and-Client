ff41a1dac7ba20d24319ef91ae0d3948c60db5fc
TerryLikesCoding <18328621355@163.com>
2023-05-30 13:17:15 +0800

    上传插件页面初稿

--- client/src/components/Upload.vue
+++ client/src/components/Upload.vue
@@ -1,48 +1,70 @@
-
 <template>
-  <div>
-    <h1>Upload Extension</h1>
+  <div class="container">
+    <h3>上传扩展</h3>
     <form @submit.prevent="uploadExtension">
-      <label for="name">Name:</label>
-      <input type="text" id="name" v-model="name" required>
-      <label for="version">Version:</label>
-      <input type="text" id="version" v-model="version" required>
-      <label for="description">Description:</label>
-      <textarea id="description" v-model="description"></textarea>
-      <label for="createdBy">Created by:</label>
-      <input type="text" id="createdBy" v-model="createdBy" required>
-      <button type="submit">Upload</button>
+      <div class="form-group">
+        <label for="name">名称:</label>
+        <input type="text" class="form-control" id="name" v-model="name">
+      </div>
+      <div class="form-group">
+        <label for="version">版本号:</label>
+        <input type="text" class="form-control" id="version" v-model="version">
+      </div>
+      <div class="form-group">
+        <label for="description">描述:</label>
+        <textarea class="form-control" id="description" v-model="description"></textarea>
+      </div>
+      <div class="form-group">
+        <label for="file">上传扩展:</label>
+        <div class="dropzone border p-2" v-bind:class="{ 'drag-over': isDragging }"
+             @dragover.prevent="isDragging = true" @dragleave.prevent="isDragging = false"
+             @drop.prevent="handleDrop($event)">
+          <div v-if="!selectedFile">
+            将文件拖放到此处，或点击此处选择要上传的文件。
+          </div>
+          <div v-else>
+            <strong>已选择文件:</strong> {{ selectedFile.name }}
+          </div>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary">上传</button>
     </form>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-
 export default {
   data() {
     return {
       name: '',
       version: '',
       description: '',
-      createdBy: '',
+      selectedFile: null,
+      isDragging: false,
     };
   },
   methods: {
+    handleDrop(event) {
+      this.selectedFile = event.dataTransfer.files[0];
+      this.isDragging = false;
+    },
     async uploadExtension() {
-      try {
-        const response = await axios.post('/extensions/upload', {
-          name: this.name,
-          version: this.version,
-          description: this.description,
-          createdBy: this.createdBy,
-        });
-        alert(`Extension uploaded with ID: ${response.data.id}`);
-        this.$router.push('/');
-      } catch (error) {
-        console.error(error);
-      }
+      // TODO: 实现上传扩展的逻辑
     },
   },
 };
 </script>
+
+<style>
+.dropzone {
+  width: 100%;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+}
+.drag-over {
+  border-color: blue;
+}
+</style>
